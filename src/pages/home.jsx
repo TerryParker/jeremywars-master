9fe466faf0c8a75ebda835564e986dce826669dc
Terry Parker <terry.parker@selu.edu>
2020-06-13 14:34:43 -0500

    basic form set up that will take in a input of a character. Won't submit if blank and gives just a alert message once submit button is clicked. No access to the API yet.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 function Home () {
+
+    const handleSubmit = event => {
+        alert("You did it!");
+    }
+
     return (
+        <>
         <h1>Star Wars Character</h1>
+        <div style={{ margin: 12}}>
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>Name of Character: </Form.Label>
+                <Form.Control 
+                required
+                type="name"
+                placeholder="Luke Skywalker"
+                />
+                {' '}
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+        </div>
+        </>
     );
 }
 
