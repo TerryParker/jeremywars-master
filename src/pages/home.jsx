3777063f94ad83a48178fae129586618d0cb7ddf
Terry Parker <terry.parker@selu.edu>
2020-06-14 07:56:54 -0500

    setup the background through the index.html body tag to black to match the star wars opening credits. Then edited the alignment of the text to be center within the home file, while changing the color with in line styling for visibility.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -20,64 +20,66 @@ function Home () {
             console.log(json);
             setCharacter( json.results);
         }));
-        console.log(character.created);
     }
     
     return (
         <>
-        
-        <h1>Star Wars Character</h1>
-        <div style={{ margin: 12}}>
-            <Form onSubmit={handleSubmit}>
-                <Form.Label>Name of Character: </Form.Label>
-                <Form.Control 
-                required
-                type="name"
-                id="1"
-                placeholder="Luke Skywalker"
-                />
-                {' '}
-                <Button variant="primary" type="submit">Submit</Button>
-            </Form>
-            <div>
-            {
-                character.map(val =>(
-                    <ListGroup key={character}>
-                    <ListGroup.Item key={val.name}>
-                        <h1>
-                            
-                            {val.name}
-                        </h1>
-                    </ListGroup.Item>
-                    <ListGroup.Item key={val.height}>
-                        <div>
-                            Height: {' '}
-                            {val.height}
-                        </div>
-                    </ListGroup.Item>
-                    <ListGroup.Item key={val.hair_color}>
-                        <div>
-                            Hair Color: {' '}
-                            {val.hair_color}
-                        </div>
-                    </ListGroup.Item>
-                    <ListGroup.Item key={val.gender}>
-                        <div>
-                            Gender: {' '}
-                            {val.gender}
-                        </div>
-                    </ListGroup.Item>
-                    <ListGroup.Item key={val.created}>
-                        <div>
-                            Created: {' '}
-                            {val.created.substr(0,10)}
-                        </div>
-                    </ListGroup.Item>
-                    </ListGroup>
-                )
-                    )
-            }
-            </div>
+        <div>
+            <center>
+            <h1 style={{color: "yellow"}}>Star Wars Character</h1>
+                <div >
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Label style={{color: "yellow"}}>Name of Character: </Form.Label>
+                        <Form.Control 
+                        required
+                        type="name"
+                        id="1"
+                        placeholder="Luke Skywalker"
+                        />
+                        {' '}
+                        <Button variant="primary" type="submit">Submit</Button>
+                    </Form>
+                    <div>
+                    {
+                        character.map(val =>(
+                            <ListGroup key={character}>
+                            <ListGroup.Item key={val.name}>
+                                <h1 style={{color: "yellow"}}>
+                                    
+                                    {val.name}
+                                </h1>
+                            </ListGroup.Item>
+                            <ListGroup.Item key={val.height}>
+                                <div style={{color: "yellow"}}>
+                                    Height: {' '}
+                                    {val.height}
+                                </div>
+                            </ListGroup.Item>
+                            <ListGroup.Item key={val.hair_color}>
+                                <div style={{color: "yellow"}}>
+                                    Hair Color: {' '}
+                                    {val.hair_color}
+                                </div>
+                            </ListGroup.Item>
+                            <ListGroup.Item key={val.gender}>
+                                <div style={{color: "yellow"}}>
+                                    Gender: {' '}
+                                    {val.gender}
+                                </div>
+                            </ListGroup.Item>
+                            <ListGroup.Item key={val.created}>
+                                <div style={{color: "yellow"}}>
+                                    Created: {' '}
+                                    {val.created.substr(0,10)}
+                                </div>
+                            </ListGroup.Item>
+                            </ListGroup>
+                        )
+                            )
+                    }
+                    </div>
+                </div>
+            </center>
         </div>
         </>
     );
