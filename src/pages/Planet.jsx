3946950f7ee9ec8f28284db69c39760800c82e31
Terry Parker <terry.parker@selu.edu>
2020-06-15 12:59:07 -0500

    server can now handle searching planets by name and there is a route set up for the new page for planets. Need to add vehicles to the server next with a page and then work on possibly a navbar for choosing between which page of searching the user would prefer.

---
+++ src/pages/Planet.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PlanetSearch from '../components/PlanetSearch';
+
+function Planet () {
+    return (
+        <>
+        <PlanetSearch/>
+        </>
+    )
+}
+export default Planet;
