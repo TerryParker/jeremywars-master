e3cdf5a0d99bbb8fa877baa3dd060c69b5e9b60f
Terry Parker <47099716+TerryParker@users.noreply.github.com>
2020-06-15 16:48:06 -0500

    Character has its own page now and home page just holds the info of what the app is instead of doing a Terrynary on showing the character page or not. (#19)
    Co-authored-by: Terry Parker <terry.parker@selu.edu>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ function NavigationBar () {
         <Navbar expand="lg" variant="dark" bg="dark" fixed="top">
             <Navbar.Brand href="/home">Jeremy Wars</Navbar.Brand>
             <Nav className="ml-auto">
-                <Nav.Link href="/home">Characters</Nav.Link>
+                <Nav.Link href="/character">Characters</Nav.Link>
                 <Nav.Link href="/planet">Planets</Nav.Link>
                 <Nav.Link href="/vehicle">Vehicles</Nav.Link>
                 <Nav.Link href="/species">Species</Nav.Link>
---
+++ src/pages/Character.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CharacterSearch from '../components/CharacterSearch';
+
+function Character () {
+    return (
+        <>
+        <CharacterSearch/>
+        </>
+    )
+}
+export default Character;
