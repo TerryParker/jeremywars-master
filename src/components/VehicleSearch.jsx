2d5a0ea3993515a90c7b215f9b9d08d2f071ad4b
Terry Parker <terry.parker@selu.edu>
2020-06-15 17:06:39 -0500

    edited the home page to have buttons to bring to the separate search pages with a welcome message. Also edited the top padding of other pages so the title was no longer being covered by the navBar. Next will aim to update the form/buttons on each search page.

--- src/components/VehicleSearch.jsx
+++ src/components/VehicleSearch.jsx
@@ -33,7 +33,7 @@ function VehicleSearch () {
     
     return (
         <>
-        <div className="starwarsFont">
+        <div className="starwarsFont top-space">
             <center>
             <div style={{color: "yellow", fontSize:"70px"}}>Star Wars </div>
                 <div >
